5c64298d92ecf56545b27b283ea700f236d402d6
wupeifen <peifensg@gmail.com>
2018-08-13 23:40:29 +0800

    create todo item

--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,48 @@
 import React, { Component} from "react";
 
 class Todo extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            item: []
+        };
+
+        this.addTodo = this.addTodo.bind(this);
+    }
+
+    addTodo(e) {
+        if (this._inputElement.value !== "") {
+            var newItem = {
+              text: this._inputElement.value,
+              key: Date.now()
+            };
+         
+            this.setState((prevState) => {
+              return { 
+                items: prevState.items.concat(newItem) 
+              };
+            });
+           
+            this._inputElement.value = "";
+          }
+           
+          console.log(this.state.items);
+             
+          e.preventDefault();
+
+    }
+
+
     render() {
         return (
             <div>
                 <h1>Todo</h1>
                 <div className="todo">
                 <div className="header">
-                <form>
-                    <input placeholder="Todo Item"></input>
+                <form onSubmit={this.addTodo}>
+                    <input  ref={ (a)=> this._inputElement = a}
+                    placeholder="Todo Item"></input>
                     <button type="submit">Add</button>
                 </form>
                 </div>
